0f81001718c35f9986996b802ae2ceeeb0d37d8d
Txreq <txreqb2w.pub@skiff.com>
2024-01-11 15:40:31 +0100

    full support for the LGBTQ++JavascriptReact Community <3

--- donut.f90
+++ donut.f90
@@ -1,20 +1,20 @@
 program donut
   implicit none
-  real :: A = 1.0
-  real :: B = 2.0
+  real :: A = 1.5
+  real :: B = 1.5
 
+    call render_frame(A, B)
   do  
     call system("clear")
     call render_frame(A, B)
-    ! call sleep(1)
-    A = A + 0.01
-    B = B + 0.04
+    ! A = A + 0.01
+    ! B = B + 0.04
   end do
 contains
   subroutine render_frame(A,B)
     implicit none
-    integer, parameter :: SCREEN_WIDTH = 45
-    integer, parameter :: SCREEN_HEIGHT = 105  
+    integer, parameter :: SCREEN_WIDTH = 50
+    integer, parameter :: SCREEN_HEIGHT = 100
     real :: PI = 4.0 * atan(1.0)
     real :: THETA_SPACING = 0.08
     real :: PHI_SPACING = 0.01
@@ -31,18 +31,19 @@ contains
     real :: R1, R2, K2
     real :: K1
 
-    integer, dimension(SCREEN_WIDTH,SCREEN_HEIGHT) :: zbuffer
     character, dimension(SCREEN_WIDTH,SCREEN_HEIGHT) :: output 
-    
-    integer :: characters(12)
+    integer, dimension(SCREEN_WIDTH,SCREEN_HEIGHT) :: zbuffer = 0
+    integer, dimension(SCREEN_WIDTH,SCREEN_HEIGHT) :: shades = 0
+    integer :: colors(12)
+    character(len=12) :: characters = ".,-~:;=!*#$@"
 
     integer :: i 
     integer :: j
+    character(len=10) :: color
 
-    zbuffer = 0 
-    output = " " 
-    characters = [46, 44, 45, 126, 58, 59, 61, 33, 42, 35, 36, 64]
-    
+    output = " "
+    colors = [53, 54, 55, 56, 57, 90, 91, 92, 93, 127, 128, 129]
+
     R1 = 1 
     R2 = 2
     K2 = 120
@@ -82,9 +83,10 @@ contains
           if (lum > 0) then
             if (az > zbuffer(xp, yp)) then
               zbuffer(xp, yp) = az            
-              lum_idx = int(lum * 8) 
-              if (lum_idx < size(characters)) then
-                output(xp, yp) = achar(characters(lum_idx + 1))
+              lum_idx = int(lum * 8)
+              if(lum_idx >= 0 .and. lum_idx < 12) then
+                output(xp, yp) = characters(lum_idx+1:lum_idx+1)
+                shades(xp, yp) = colors(lum_idx + 1) 
               end if
             end if
           end if 
@@ -94,7 +96,8 @@ contains
 
     do i = 1, SCREEN_WIDTH, 1
       do j = 1, SCREEN_HEIGHT, 1
-        write(*, '(A)', advance='no') output(i, j)
+        write(color, '(I0)') shades(i,j)
+        write(*,'(A)',advance='no') achar(27)//'[38;5;'//trim(color)//'m'//output(i,j)//achar(27)//'[0m'
       end do
       print *, "" 
     end do
